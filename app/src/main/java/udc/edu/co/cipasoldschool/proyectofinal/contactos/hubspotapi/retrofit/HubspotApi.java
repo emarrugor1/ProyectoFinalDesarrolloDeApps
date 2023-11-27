fd3c8c4d038a933b8fbfa84bfb7b8f7ed7306cec
emarrugo <emarrugo@proteccion.com.co>
2023-11-26 20:43:29 -0500

    Se implementa el formulario de inserciones

--- app/src/main/java/udc/edu/co/cipasoldschool/proyectofinal/contactos/hubspotapi/retrofit/HubspotApi.java
+++ app/src/main/java/udc/edu/co/cipasoldschool/proyectofinal/contactos/hubspotapi/retrofit/HubspotApi.java
@@ -1,7 +1,5 @@
 package udc.edu.co.cipasoldschool.proyectofinal.contactos.hubspotapi.retrofit;
 
-import java.util.List;
-
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
@@ -9,14 +7,14 @@ import retrofit2.http.Headers;
 import retrofit2.http.POST;
 import retrofit2.http.Query;
 import udc.edu.co.cipasoldschool.proyectofinal.contactos.hubspotapi.request.CreateContactRequest;
-import udc.edu.co.cipasoldschool.proyectofinal.contactos.model.Contact;
+import udc.edu.co.cipasoldschool.proyectofinal.contactos.hubspotapi.response.CreateContactResponse;
 import udc.edu.co.cipasoldschool.proyectofinal.contactos.model.Contacts;
 
 public interface HubspotApi {
 
     @Headers({"Content-Type: application/json","Authorization: Bearer ***REMOVED***"})
     @POST("crm/v3/objects/contacts")
-    Call<Void> createContact(@Body CreateContactRequest contacto);
+    Call<CreateContactResponse> createContact(@Body CreateContactRequest contacto);
 
     @Headers({"Content-Type: application/json","Authorization: Bearer ***REMOVED***"})
     @GET("crm/v3/objects/contacts")
